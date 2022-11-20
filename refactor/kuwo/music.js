0b14546c2865d3218715b3ed203c2bbb4ea402f2
codelang <codelang29@gmail.com>
2022-11-20 13:10:52 +0800

    kuwo

---
+++ refactor/kuwo/music.js
@@ -0,0 +1,57 @@
+const kuwo = require('./kuwo');
+
+const provider = [kuwo]
+
+const music = provider[0]
+
+
+function search() {
+    const url = `/search?keywords=不能说的秘密&curpage=1&type=0`;
+
+    // todo 搜索，酷我需要本地的 cookie 有 token 才行
+    music.search(url).success(function (data) {
+        // 
+        const searchTrack = data.result[0];
+
+        console.log(searchTrack)
+        // console.log(data.result.length)
+
+        // todo 获取音频 mp3
+        music.bootstrap_track(searchTrack, (response) => {
+            let url = response.url;
+            let bitrate = response.bitrate;
+            let platform = response.platform;
+            console.log("url--->" + url)
+            console.log("bitrate--->" + bitrate)
+            console.log("platform--->" + platform)
+            // reject(sound); // Use Reject to return immediately
+        }, function () {
+            console.log("error--->")
+        })
+
+
+        // todo 获取歌词 
+        const url = "lyric?lyric_url=" + searchTrack.lyric_url
+        console.log(url)
+        music.lyric(url).success((lyric) => {
+            console.log(lyric)
+        })
+
+
+        // todo 显示推荐歌单
+        const list_url = `/playlist?offset=0`;
+        music.show_playlist(list_url).success((data) => {
+            console.log(data.result[0])
+
+            // todo 获取歌单的歌单曲目，请求返回的是 html，报浏览器版本低，怎么解？
+            const d = data.result[0]
+            const url = `/playlist?list_id=` + d.id;
+            music.get_playlist(url).success((playlist) => {
+                console.log(playlist)
+            })
+        })
+    })
+}
+
+
+search()
