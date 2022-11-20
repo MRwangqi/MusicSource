5ce947ed5b0e1f09c8622fc72c946fa6af637f51
codelang <codelang29@gmail.com>
2022-11-20 22:24:33 +0800

    webpack

--- refactor/taihe/webpack.config.js
+++ refactor/taihe/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
     entry: './src/music.js',
@@ -10,9 +11,28 @@ module.exports = {
     },
     plugins: [
         new CleanWebpackPlugin(),
-        new NodePolyfillPlugin()
+        new NodePolyfillPlugin(),
+        new UglifyJsPlugin({
+            uglifyOptions: {
+                //生产环境自动删除 console
+                compress: {
+                    drop_debugger: true,
+                    drop_console: true,
+                    pure_funcs: ['console.log']
+                }
+            },
+            chunkFilter: (chunk) => {
+                // `search` 块不压缩
+                if (chunk.name === 'search') {
+                    return false;
+                }
+                return true;
+            },
+            sourceMap: false,
+            parallel: true,
+        })
     ],
     optimization: {
-        minimize: false,
+        minimize: true,
     }
 }
